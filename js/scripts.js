dafee1b61eb2a6bf789eb0810c0d75fa1bdc4356
MARA TAMIRIS MIRANDA DE OLIVEIRA <43901448802@jpmorgan.sp>
2018-01-31 21:53:34 -0200

    ha

--- js/scripts.js
+++ js/scripts.js
@@ -38,21 +38,21 @@ function atualizarSecao(secao) {
     for (var posicao = 0; posicao < listaNotas.contaItens(); posicao++) {
         var notaAtual = listaNotas.pegaNota(posicao);
         if (notaAtual.editando) {
-            conteudoSecao += '<form class="note">'+
-                                '<input class="note__title" type="text" name="titulo" value="' + notaAtual.titulo + '" placeholder="Título">'+
-                                '<textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' + notaAtual.texto +'</textarea>'+
-                                '<button class="note__control" type="button" onclick="atualizaNota(this.form.titulo, this.form.texto, this.form, ' + posicao +')">'+
-                                    'Concluído'+
-                                '</button>'+
-                             '</form>';
+            conteudoSecao += `<form class="note">
+                                <input class="note__title" type="text" name="titulo" value="${notaAtual.titulo}" placeholder="Título">
+                                <textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">${notaAtual.texto}</textarea>
+                                <button class="note__control" type="button" onclick="atualizaNota(this.form.titulo, this.form.texto, this.form, ${posicao})">
+                                    ${Concluído}
+                                </button>
+                             </form>`;
         } else {
-            conteudoSecao += '<form class="note" onclick="editaFormulario(' + posicao + ')">'+
-                                '<button class="note__control" type="button" onclick="removerNota(event, ' + posicao + ')">'+
-                                    '<i class="fa fa-times" aria-hidden="true"></i>'+
-                                '</button>'+
-                                '<h1 class="note__title">' + notaAtual.titulo + '</h1>'+
-                                '<p class="note__body">' + notaAtual.texto + '</p>'+                             
-                             '</form>';
+            conteudoSecao += `<form class="note" onclick="editaFormulario(${posicao})">
+                                <button class="note__control" type="button" onclick="removerNota(event,${posicao})">
+                                    <i class="fa fa-times" aria-hidden="true"></i>
+                                </button>
+                                <h1 class="note__title">${notaAtual.titulo}</h1>
+                                <p class="note__body">${notaAtual.texto}</p>                             
+                             </form>`;
         }
     }
 
@@ -68,7 +68,7 @@ var listaNotas ={
     secao: document.getElementsByClassName("notes")[0],
     // var listaInterna = [];
     listaInterna: [] ,
-    adiciona: function(titulo, texto){
+    adiciona:(titulo, texto)=>{
         var nota = {
             titulo: titulo,
             texto: texto,
@@ -78,24 +78,24 @@ var listaNotas ={
         this.listaInterna.push(nota);
         atualizarSecao(this.secao);
     },
-    remove: function(posicao){
+    remove: (posicao) =>{
         this.listaInterna.splice(posicao, 1);
         atualizarSecao(this.secao);
     },
-    edita: function(posicao){
+    edita: (posicao) =>{
         this.listaInterna[posicao].edita = true;
         atualizarSecao(this.secao);
     },
-    salva: function(posicao, novoTitulo, novoTexto){
+    salva: (posicao, novoTitulo, novoTexto)=>{
         this.listaInterna[posicao].titulo = novoTitulo;
         this.listaInterna[posicao].texto = novoTexto;
         this.listaInterna[posicao].editando = false;
         atualizarSecao(this.secao);
     },
-    pegaNota: function (posicao){
+    pegaNota:(posicao)=>{
         return this.listaInterna[posicao];
     },
-    contaItens: function(){
+    contaItens: ()=>{
         return this.listaInterna.length;
     },
 }
